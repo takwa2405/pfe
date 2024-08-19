62c3e22eeb1631adf047db68921c56850a0a5a15
takwa2405 <takwa.haggui@esprit.tn>
2024-08-19 10:32:32 +0200

    apisteps

---
+++ src/test/java/StepsDefinitions/apiStep.java
@@ -0,0 +1,44 @@
+package StepsDefinitions;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
+import static org.junit.Assert.assertEquals;
+import java.util.HashMap;
+import java.util.Map;
+public class apiStep {
+	
+	 apiClient apiclient = new apiClient();
+	  
+	    Response response;
+
+	    @When("j'envoie les données à l'API predictTestResult")
+	    public void sendPredictTestResult() {
+	        Map<String, Object> requestBody = new HashMap<>();
+	        requestBody.put("Username", "validuser123");
+	        requestBody.put("Password", "ValidPass!2024");
+	        requestBody.put("Is Identifier Valid", "True");
+	        requestBody.put("Is Password Correct", "True");
+	        requestBody.put("Is Account Disabled", "True");
+	        requestBody.put("Is Identifier Empty", "False");
+	        requestBody.put("Is Password Empty", "False");
+	        requestBody.put("Expected Result", "Pass");
+	        requestBody.put("Rejection Reason", "None");
+
+	        response = apiclient.postPredictTestResult(requestBody);
+	        System.out.print(response);
+	    }
+
+	    @And("la réponse de l'API doit avoir le statut {int}")
+	    public void verifyApiResponse(int expectedStatusCode) {
+	        assertEquals(expectedStatusCode, response.getStatusCode());
+	        System.out.print(response.getBody());
+	    }
+
+	  
+	    @Then("je imprime la réponse de l'API")
+	    public void printApiResponse() {
+	        String responseBody = response.getBody().asString();
+	        System.out.println("Réponse de l'API: " + responseBody);
+	    }
+}

dd036716f64d53515d0a61705dbd90ab00524034
takwa2405 <takwa.haggui@esprit.tn>
2024-08-21 14:13:48 +0200

    hookss

---
+++ src/test/java/StepsDefinitions/Hooks.java
@@ -0,0 +1,30 @@
+package StepsDefinitions;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+  @Before
+    public  void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.get("http://192.168.0.21:7070/ESBOnline/Online/default.aspx");
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}

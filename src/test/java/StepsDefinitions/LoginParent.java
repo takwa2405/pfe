6e4291be7cd7dddc6ab114ccff0c5889fe730421
takwa2405 <takwa.haggui@esprit.tn>
2024-08-22 12:33:05 +0200

    verifier vos parametre

--- src/test/java/StepsDefinitions/LoginParent.java
+++ src/test/java/StepsDefinitions/LoginParent.java
@@ -2,13 +2,14 @@ package StepsDefinitions;
 
 import java.time.Duration;
 
-import org.junit.After;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -100,8 +101,20 @@ public class LoginParent {
 	    @Then("message d'erreur verifier vos parametres")
 	    public void un_message_d_erreur_indiquant_que_l_identifiant_est_incorrect() {
 	    	 
-	        Assert.assertTrue( driver.switchTo().alert().getText().contains("Verifier vos paramètres"));
-	        driver.switchTo().alert().accept();
+	    	try {
+	            // Attendre jusqu'à 10 secondes pour que l'alerte soit présente
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+	            
+	            // Vérifier que le texte de l'alerte contient "Verifier vos paramètres."
+	            Assert.assertTrue(alert.getText().contains("Verifier vos paramètres."));
+	            
+	            // Accepter (fermer) l'alerte
+	            alert.accept();
+	        } catch (TimeoutException | NoAlertPresentException e) {
+	            System.out.println("Aucune alerte n'est apparue dans le délai imparti ou l'alerte n'était pas présente.");
+	            Assert.fail("Aucune alerte trouvée.");
+	        }
 	    }
 	    
 	    @Then("um message d'erreur Erreur du serveur dans l'application")

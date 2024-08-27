7c88609c966f48f668432f2197fb463ec916e6b7
takwa2405 <47181112+takwa2405@users.noreply.github.com>
2024-08-27 15:19:20 +0200

    Update Hooks.java

--- src/test/java/StepsDefinitions/Hooks.java
+++ src/test/java/StepsDefinitions/Hooks.java
@@ -13,12 +13,17 @@ public class Hooks {
     public static WebDriverWait wait;
   @Before
     public  void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-search-engine-choice-screen");
+       ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Activer le mode headless
+        options.addArguments("--disable-search-engine-choice-screen"); // Autres arguments optionnels
+        options.addArguments("--no-sandbox"); // Pour éviter des problèmes liés au sandbox
+        options.addArguments("--disable-dev-shm-usage"); // Pour éviter des erreurs de mémoire partagée
+        options.addArguments("--disable-gpu"); // Désactiver l'accélération GPU
+
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         driver.get("http://192.168.0.21:7070/ESBOnline/Online/default.aspx");
-        driver.manage().window().maximize();
+        driver.manage().window().maximize(); // Peut être inutile en mode headless, mais n'affecte pas
     }
 
     @After
